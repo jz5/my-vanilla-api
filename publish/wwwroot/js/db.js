0b6e9b67097e55aad76e2d8f9d856004d1bfe40b
jz5 <jz5@hotmail.com>
2020-07-08 00:38:25 +0900

    test

---
+++ publish/wwwroot/js/db.js
@@ -0,0 +1,22 @@
+﻿window.getFoo = (count) =>
+{
+    return "a".repeat(count);
+}
+
+
+var db = new Dexie("friend_database");
+db.version(2).stores({
+    friends: 'name,shoeSize'
+});
+
+window.putFriend = (friend) =>
+    db.friends.put(friend);
+
+window.getFriends = () =>
+    db.friends.toArray();
+
+window.getFriend = (name) =>
+    db.friends.get(name);
+
+
+
